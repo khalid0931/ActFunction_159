d6ba0aa69ffb2710a52b03d990ad6389e4ebf75d
khalid0931 <mutiarif53@gmail.com>
2026-03-02 14:54:51 +0700

    menambahkan void input

--- luas lingkaran.cpp
+++ luas lingkaran.cpp
@@ -1,5 +1,14 @@
 //library
 #include <iostream>
 using namespace std;
+
 //deklarsi variable global
 int phi, r;
+
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "masukan phi : ";
+    cin >> phi;
+    cout << "masukan r";
+    cin >> r;
+}
